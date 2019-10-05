e47a2881663c7efaa670e37d29c6ab5f67130be0
Nelson Leung <nelson.leung@Nelsons-Air.fios-router.home>
2019-10-05 00:38:28 -0400

    routes and predictions

--- app.py
+++ app.py
@@ -36,5 +36,48 @@ def make_predictions():
      """Returns prediction page"""
      return render_template("predictions.html")
 
+
+def predictPlay(inputValue):
+    input = []
+     for value in inputvalue.values():
+          input.append(value)
+          
+     input_df = pd.DataFrame(input)
+     input_df.rename(columns = {0:'input_value'}, inplace=True)
+     to_predict = input_df.transpose() # or df1.transpose()
+     loaded_model = joblib.load('resources/next_play_predictor_LogReg.pkl')
+     # loaded_model = pickle.load(open("/resoureces/next_play_predictor_LogReg.pkl","rb"))
+     result = loaded_model.predict(to_predict)
+     
+     return result
+
+@app.route('/result', methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        
+        # any transforms
+        modelValue = request.form.to_dict()
+        modelValue = list(modelValue.values())
+        modelValue = list(map(int, modelValue))
+
+        prediction = predictPlay(modelValue)
+     
+        return render_template("predictions.html",prediction=prediction)
+
+
+# @app.route("/score")
+# def score():
+#     print(request.args)
+#     if(request.args):
+#         x_input, predictions = make_prediction(request.args(modelValue))
+#         print(x_input)
+#         return render_template('predictions.html', response=prediction)
+#     else: 
+#         #For first load, request.args will be an empty ImmutableDict
+#         # type. If this is the case we need to pass an empty string
+#         # into make_prediction function so no errors are thrown.
+#         x_input, predictions = make_prediction('')
+#         return render_template('predictions.html', response=null)
+
 if __name__ == "__main__":
      app.run()
